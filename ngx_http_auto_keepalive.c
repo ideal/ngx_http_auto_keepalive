2e8b86bb2121d57dd01e307c2bbf55f653cc1886
ideal <idealities@gmail.com>
2013-09-21 19:54:53 +0800

    add code

--- ngx_http_auto_keepalive.c
+++ ngx_http_auto_keepalive.c
@@ -38,6 +38,9 @@ static void *create_http_auto_keepalive_loc_conf(ngx_conf_t *cf);
 static char *merge_http_auto_keepalive_loc_conf(ngx_conf_t *cf,
                 void *parent, void *child);
 
+static ngx_int_t ngx_http_auto_keepalive_init(ngx_conf_t *cf);
+static ngx_int_t ngx_http_auto_keepalive_handler(ngx_http_request_t *r);
+
 static ngx_inline u_char *ngx_strrlchr(
                           u_char *p, u_char *last, u_char c)
 {
@@ -125,5 +128,55 @@ static char *merge_http_auto_keepalive_loc_conf(ngx_conf_t *cf,
                          prev->keepalive_autoclose,
                          0);
 
+    if (conf->keepalive_autoclose) {
+        ngx_http_auto_keepalive_init(cf);
+    }
+
     return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_http_auto_keepalive_init(ngx_conf_t *cf)
+{
+    ngx_http_handler_pt       *handler;
+    ngx_http_core_main_conf_t *cmcf;
+
+    cmcf     = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+    if (cmcf->phases[NGX_HTTP_POST_ACCESS_PHASE].handlers.pool == NULL) {
+        return NGX_OK;
+    }
+
+    handler  = ngx_array_push(&cmcf->phases[NGX_HTTP_POST_ACCESS_PHASE].handlers);
+    if (handler == NULL) {
+        return NGX_ERROR;
+    }
+    *handler = ngx_http_auto_keepalive_handler;
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_http_auto_keepalive_handler(ngx_http_request_t *r)
+{
+    ngx_http_auto_keepalive_loc_conf_t  *conf;
+    u_char *ext;
+
+    conf = ngx_http_get_module_loc_conf(r, ngx_http_auto_keepalive_module);
+    if (conf->keepalive_autoclose && r->headers_in.referer) {
+        /*
+         * if keepalive_autoclose is enabled, and the request is for
+         * specific file type and has referer, then it will auto
+         * disable keep-alive.
+         */
+        ext = (u_char *)ngx_strrlchr(r->uri.data, r->uri.data+r->uri.len, '.');
+        if (ext
+            && (ngx_strncmp(ext, ".gz", 3) == 0
+                || ngx_strncmp(ext, ".bz2", 4) == 0
+                || ngx_strncmp(ext, ".zip", 4) == 0
+                || ngx_strncmp(ext, ".rar", 4) == 0
+                || ngx_strncmp(ext, ".iso", 4) == 0))
+        {
+            r->keepalive = 0;
+        }
+    }
+
+    return NGX_OK;
 }
